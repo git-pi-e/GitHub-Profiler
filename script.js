69a2d946dc0694989b684696bbc55fab58f26043
arjun <arjunputhli00@gmail.com>
2022-06-12 18:52:57 +0530

    search bar functional

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let url = 'https://api.github.com/users/';
+const main = document.getElementById("main");
+let form = document.getElementById("form");
+let search = document.getElementById("search");
 
 async function getUser(userid){
     let resp = await fetch(url+userid)
     let user_json_data = await resp.json();
-    //displayUser(user_json_data);
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const user = search.value;
+    if (user){
+        getUser(user);
+        console.log(user);
+        search.value = "";
+    }
+});
